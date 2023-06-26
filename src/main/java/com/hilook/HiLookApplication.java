a9e1db4b75c0c0bae2fdd4cc4f371ca96e7e2fd8
jaei <ieajnowk@gmail.com>
2023-06-26 19:37:55 +0900

    Spring

---
+++ src/main/java/com/hilook/HiLookApplication.java
@@ -0,0 +1,12 @@
+package com.hilook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiLookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiLookApplication.class, args);
+	}
+}
