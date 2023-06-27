c0f4b02f0b68d0efc495a67936d25fb497b3c8c0
jaei <ieajnowk@gmail.com>
2023-06-27 10:26:21 +0900

    Spring

---
+++ src/test/java/com/hilook/HiLookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiLookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
